bb8b71817c506cb8337f26ec2cb6d0d7d84f2b27
Saj1234 <saj1234@gmail.com>
2024-08-05 12:38:25 +1000

    feat(qna.py): add interactive Q&A session functionality refactor(qna.py): replace sys module with uuid for session ID generation
    This commit introduces an interactive Q&A session feature that allows
    users to ask questions in a loop until they choose to exit. The
    session ID is now generated using the uuid module instead of relying
    on command-line arguments, improving usability and ensuring a unique
    session ID for each run.

--- app/qna.py
+++ app/qna.py
@@ -1,9 +1,10 @@
-import sys
+import uuid
 from agents import load_agent_executor
 from chat_history import load_chat_history, save_chat_history
 
 
 agent_executor = load_agent_executor()
+
 
 def answer_question(session_id, user_input):
     """Function process QNA session. This function will load chat history for passed session id if available."""
@@ -14,15 +15,29 @@ def answer_question(session_id, user_input):
         {"input": user_input, "chat_history": chat_history}
     )
     response_output = response["output"]
-
     chat_history.append(response_output)
     save_chat_history(session_id, chat_history)
 
     return response_output
 
-if __name__ == '__main__':
-    if sys.argv[1:] and sys.argv[2:]:
-        answer_question(sys.argv[1], sys.argv[2])
-    else:
-        print("empty args")
-    
+
+def q_and_a(session_id):
+    """Function Qna Chat session"""
+    while True:
+        # Prompt user for input
+        question = input("Question: ")
+
+        # Break the loop if 'q' is pressed
+        if question.lower() == "quit" or question.lower() == "exit":
+            print("Exiting session")
+            break
+
+        answer = answer_question(session_id, question)
+        print("Answer: ", answer)
+
+
+if __name__ == "__main__":
+
+    SESSION_ID = str(uuid.uuid1())
+    print(SESSION_ID)
+    q_and_a(SESSION_ID)
