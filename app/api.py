e1ca87d3b48bffe3b27d97bb4d0cff400b45f841
Saj1234 <saj1234@gmail.com>
2024-08-30 12:17:43 +1000

    feat(api.py): implement basic API with ping and ask endpoints refactor(app.py): move Q&A logic from qna.py to app.py and enhance structure chore(qna.py): remove obsolete qna.py file as its functionality is ref

---
+++ app/api.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, abort
+from app import answer_question
+
+# create a Flask instance
+app = Flask(__name__)
+
+@app.route('/ping', methods=['GET', 'POST'])
+def ping(): 
+    """Function Ping - This function will be used to check API connectivity"""
+    return 'Pong!'
+
+@app.route('/ask', methods=['POST'])
+def ask():
+    """Function ask - Ask questions from LLM and session id will be used to load the chat from history if available."""
+    if(request.method == 'POST'): 
+        request_data = request.get_json(force=True)
+        # check if properties exist in the request body first
+        if "session_id" in  request_data and "question" in  request_data:
+            session_id = request_data["session_id"]
+            question = request_data["question"]
+            answer = answer_question(session_id, question)
+            return answer
+
+        return abort(500, "'session_id' or 'question' parameter missing.") 
+
+    return abort(500, "Invalid request.") 
--- app/app.py
+++ app/app.py
@@ -1,3 +1,43 @@
+import uuid
+from agents import load_agent_executor
+from chat_history import load_chat_history, save_chat_history
 
 
+agent_executor = load_agent_executor()
 
+
+def answer_question(session_id, user_input):
+    """Function process QNA session. This function will load chat history for passed session id if available."""
+
+    chat_history = load_chat_history(session_id)
+    # Invoke the agent and pass the current chat history
+    response = agent_executor.invoke(
+        {"input": user_input, "chat_history": chat_history}
+    )
+    response_output = response["output"]
+    chat_history.append(response_output)
+    save_chat_history(session_id, chat_history)
+
+    return response_output
+
+
+def q_and_a(session_id):
+    """Function Qna Chat session"""
+    while True:
+        # Prompt user for input
+        question = input("Question: ")
+
+        # Break the loop if 'q' is pressed
+        if question.lower() == "quit" or question.lower() == "exit":
+            print("Exiting session")
+            break
+
+        answer = answer_question(session_id, question)
+        print("Answer: ", answer)
+
+
+if __name__ == "__main__":
+
+    SESSION_ID = str(uuid.uuid1())
+    print(SESSION_ID)
+    q_and_a(SESSION_ID)
